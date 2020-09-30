fa699cc64955708d68b7fdab05df1aa82ac7577e
unknown <aline.as385@gmail.com>
2020-09-30 16:42:25 -0300

    craindo tipagens

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,20 @@
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
+
+//string, number, boolean, Array
+//interface
 
 export function helloWorld (request: Request, response: Response) {
+  const user = createUser({
+    email: 'aline.as385@gmail.com',
+    password: '123456',
+    techs: [
+      'Node',
+      "ReactJS", 
+      'React Native', 
+      { title: 'Javascript', experience: 100 },
+    ],
+  });
+
   return response.json({message: 'Hello word'});
 }
